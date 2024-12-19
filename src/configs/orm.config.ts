87ea917f63d4dbd21161a613cdb795529ff2b8da
Lucas Pereira Miranda <lucas.pereira.miranda01@gmail.com>
2024-12-19 19:36:03 -0300

    refactor: use loaded envs inside ormconfig

--- src/configs/orm.config.ts
+++ src/configs/orm.config.ts
@@ -1,24 +1,24 @@
-import * as dotenv from 'dotenv';
 import * as path from 'path';
 import { DataSource } from 'typeorm';
-dotenv.config();
+import { envConfig } from './env.load';
 
-export const config = {
+export const ormConfig = {
   type: 'postgres' as const,
-  synchronize: Boolean(process.env.TYPEORM_SYNCHRONIZE) ?? false,
-  migrationsRun: Boolean(process.env.TYPEORM_MIGRATIONS_RUN) ?? false,
-  logging: Boolean(process.env.TYPEORM_LOGGING) ?? false,
+  synchronize: envConfig.db.synchronize,
+  migrationsRun: envConfig.db.migrationsRun,
+  logging: envConfig.db.logging,
+  host: envConfig.db.host,
+  port: envConfig.db.port,
+  username: envConfig.db.username,
+  password: envConfig.db.password,
+  autoLoadEntities: envConfig.db.autoLoadEntities,
+  database: envConfig.db.database,
   entities: [`${__dirname}/**/*.entity{.ts,.js}`],
   migrations: [path.join(__dirname, '/migrations/*{.ts,.js}')],
   migrationsTableName: 'migrations',
-  host: process.env.TYPEORM_HOST ? process.env.TYPEORM_HOST : 'localhost',
-  port: Number(process.env.TYPEORM_PORT) ?? 5432,
-  username: process.env.TYPEORM_USERNAME ?? 'postgres',
-  password: process.env.TYPEORM_PASSWORD ?? 'password',
-  database: process.env.TYPEORM_DATABASE ?? 'serasa_brain_ag_challenge',
 };
 
-const datasource = new DataSource(config);
+const datasource = new DataSource(ormConfig);
 
 datasource
   .initialize()
@@ -26,7 +26,7 @@ datasource
     console.log('Data Source has been initialized!');
   })
   .catch((err) => {
-    console.error('Error during Data Source initialization', err);
+    console.error('Error during Data Source initialization!', err);
   });
 
 export default datasource;
