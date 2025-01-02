1cf4052290d46c0984a06032b5025550bcc27d1b
Lucas Pereira Miranda <lucas.pereira.miranda01@gmail.com>
2025-01-02 17:59:26 -0300

    feat: add grower resources

--- src/modules/grower/grower.controller.ts
+++ src/modules/grower/grower.controller.ts
@@ -1,27 +1,128 @@
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { GrowerService } from './grower.service';
+import { CreateGrowerRequestDto } from './dto/request/create-grower.request.dto';
+import { CreateGrowerResponseDto } from './dto/response/create-grower.response.dto';
+import {
+  UpdateGrowerBodyRequestDto,
+  UpdateGrowerParamsRequestDto,
+} from './dto/request/update-grower.request.dto';
+import { DeleteGrowerResponseDto } from './dto/response/delete-grower.response.dto';
+import { DeleteGrowerRequestDto } from './dto/request/delete-grower.request.dto';
+import { FindAllGrowerResponseDto } from './dto/response/findAll-grower.response.dto';
+import { FindOneByIdGrowerParamsRequestDto } from './dto/request/findOneById-grower.request.dto';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UpdateGrowerResponseDto } from './dto/response/update-grower.request.dto';
+import { FindOneByIdGrowerResponseDto } from './dto/response/findOneById-grower.response.dto';
+import { FindAllGrowerQueryRequestDto } from './dto/request/findAll-grower.request.dto';
 
-@Controller('grower')
+@Controller('growers')
+@ApiTags('Proprietários - Growers')
 export class GrowerController {
   constructor(private growerService: GrowerService) {}
 
   @Post()
-  async create() {
-    return;
+  @ApiOperation({ summary: 'Criação de um novo proprietário' })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Proprietário criado com sucesso',
+    type: CreateGrowerResponseDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.CONFLICT,
+    description: 'Documento do proprietário já existente',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Documento do proprietário inválido',
+  })
+  create(
+    @Body() createGrowerRequestDto: CreateGrowerRequestDto,
+  ): Promise<CreateGrowerResponseDto> {
+    return this.growerService.create(createGrowerRequestDto);
   }
 
-  @Patch()
-  async update() {
-    return;
+  @Put(':id')
+  @ApiOperation({ summary: 'Atualização de um proprietário existente' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Proprietário atualizado com sucesso',
+    type: UpdateGrowerResponseDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Proprietário não encontrado por id',
+  })
+  @ApiParam({ name: 'id', description: 'Id do proprietário', type: String })
+  async update(
+    @Param() updateGrowerParamsRequestDto: UpdateGrowerParamsRequestDto,
+    @Body() updateGrowerBodyRequestDto: UpdateGrowerBodyRequestDto,
+  ) {
+    return this.growerService.update(
+      updateGrowerParamsRequestDto,
+      updateGrowerBodyRequestDto,
+    );
   }
 
   @Get()
-  async getOne() {
-    return;
+  @ApiOperation({
+    summary:
+      'Realiza a busca paginada de todos os proprietários existentes na base',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Proprietários paginados e quantidade obtidos com sucesso',
+    type: FindAllGrowerResponseDto,
+  })
+  @Get()
+  async findAll(
+    @Query()
+    findAllGrowerQueryRequestDto: FindAllGrowerQueryRequestDto,
+  ): Promise<FindAllGrowerResponseDto> {
+    return this.growerService.findAll(findAllGrowerQueryRequestDto);
   }
 
-  @Delete()
-  async delete() {
-    return;
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Realiza a busca de um proprietário por id',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Proprietário obtido com sucesso',
+    type: FindOneByIdGrowerResponseDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Proprietário não encontrado por id',
+  })
+  @ApiParam({ name: 'id', description: 'Id do proprietário', type: String })
+  async findOneById(@Param() { id }: FindOneByIdGrowerParamsRequestDto) {
+    return this.growerService.findOneById(id);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Deleção de um proprietário existente' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Proprietário deletado com sucesso',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Proprietário não encontrado por id',
+  })
+  @ApiParam({ name: 'id', description: 'Id do proprietário', type: String })
+  delete(
+    @Param() { id }: DeleteGrowerRequestDto,
+  ): Promise<DeleteGrowerResponseDto> {
+    return this.growerService.delete(id);
   }
 }
--- src/modules/grower/grower.service.ts
+++ src/modules/grower/grower.service.ts
@@ -1,7 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Grower } from './grower.entity';
+import { CreateGrowerRequestDto } from './dto/request/create-grower.request.dto';
+import { CreateGrowerResponseDto } from './dto/response/create-grower.response.dto';
+import { isValidDocument } from '../common/utils/is-valid-document';
+import {
+  UpdateGrowerBodyRequestDto,
+  UpdateGrowerParamsRequestDto,
+} from './dto/request/update-grower.request.dto';
+import { UpdateGrowerResponseDto } from './dto/response/update-grower.request.dto';
+import { DeleteGrowerResponseDto } from './dto/response/delete-grower.response.dto';
+import { FindAllGrowerResponseDto } from './dto/response/findAll-grower.response.dto';
+import { FindAllGrowerQueryRequestDto } from './dto/request/findAll-grower.request.dto';
 
 @Injectable()
 export class GrowerService {
@@ -9,4 +25,83 @@ export class GrowerService {
     @InjectRepository(Grower)
     private growerRepository: Repository<Grower>,
   ) {}
+
+  async create(
+    createGrowerRequestDto: CreateGrowerRequestDto,
+  ): Promise<CreateGrowerResponseDto> {
+    const { document } = createGrowerRequestDto;
+
+    const alreadyExists = await this.findOneByDocument(document);
+    if (alreadyExists) {
+      throw new ConflictException(
+        `Grower with document ${document} already exists`,
+      );
+    }
+
+    const isValid = isValidDocument(document);
+    if (!isValid) {
+      throw new BadRequestException(`Document ${document} is not valid`);
+    }
+
+    return await this.growerRepository.save(createGrowerRequestDto);
+  }
+
+  async findOneByDocument(document: string): Promise<Grower> {
+    const grower = await this.growerRepository.findOne({
+      where: { document },
+    });
+    return grower;
+  }
+
+  async findAll(
+    findAllGrowerRequestDto: FindAllGrowerQueryRequestDto,
+  ): Promise<FindAllGrowerResponseDto> {
+    const { take, skip } = findAllGrowerRequestDto;
+    console.log(take, skip);
+    const [data, count] = await Promise.all([
+      this.growerRepository.find({
+        take,
+        skip,
+      }),
+      this.growerRepository.count(),
+    ]);
+
+    return {
+      data,
+      count,
+    };
+  }
+
+  async findOneById(id: string): Promise<Grower> {
+    const grower = await this.growerRepository.findOne({
+      where: { id },
+    });
+
+    if (!grower) {
+      throw new NotFoundException(`Grower with id ${id} not found`);
+    }
+
+    return grower;
+  }
+
+  async update(
+    { id }: UpdateGrowerParamsRequestDto,
+    updateGrowerRequestDto: UpdateGrowerBodyRequestDto,
+  ): Promise<UpdateGrowerResponseDto> {
+    const existentGrower = await this.findOneById(id);
+    const grower = await this.growerRepository.preload({
+      id: existentGrower.id,
+      ...updateGrowerRequestDto,
+    });
+
+    return this.growerRepository.save(grower);
+  }
+
+  async delete(id: string): Promise<DeleteGrowerResponseDto> {
+    const existentGrower = await this.findOneById(id);
+    const { affected } = await this.growerRepository.delete({
+      id: existentGrower.id,
+    });
+    return { deleted: affected > 0 };
+  }
 }
