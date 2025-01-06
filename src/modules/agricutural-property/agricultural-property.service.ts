1a41d6570405e47bc1609858c1d35d26ffd71039
Lucas Pereira Miranda <lucas.pereira.miranda01@gmail.com>
2025-01-06 18:14:46 -0300

    feat: add agricultural property service

---
+++ src/modules/agricutural-property/agricultural-property.service.ts
@@ -0,0 +1,88 @@
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AgriculturalProperty } from './agricultural-property.entity';
+import { CreateAgriculturalPropertyResponseDto } from './dto/response/create-agricultural-property.response.dto';
+import { CreateAgriculturalPropertyRequestDto } from './dto/request/create-agricultural-property.request.dto';
+import { GrowerService } from '../grower/grower.service';
+import { FindOneByIdAgriculturalPropertyResponseDto } from './dto/response/findOneById-agricultural-property.response.dto';
+import { FindAllAgriculturalPropertyQueryRequestDto } from './dto/request/findAll-agricultural-property.request.dto';
+import { FindAllAgriculturalPropertyResponseDto } from './dto/response/findAll-agricultural-property.response.dto';
+
+@Injectable()
+export class AgriculturalPropertyService {
+  constructor(
+    @InjectRepository(AgriculturalProperty)
+    private agriculturalPropertyRepository: Repository<AgriculturalProperty>,
+    @Inject(GrowerService)
+    private growerService: GrowerService,
+  ) {}
+
+  async create(
+    data: CreateAgriculturalPropertyRequestDto,
+  ): Promise<CreateAgriculturalPropertyResponseDto> {
+    const { growerId, totalArea, arableArea, vegetationArea } = data;
+
+    if (!this.isValidArea(totalArea, arableArea, vegetationArea)) {
+      throw new BadRequestException(
+        `The Arable Area and Vegetation Area combined should not be greater than the Total Area`,
+      );
+    }
+
+    const grower = await this.growerService.findOneById(growerId);
+    const agriculturalProperty = this.agriculturalPropertyRepository.create({
+      ...data,
+      grower,
+    });
+    return this.agriculturalPropertyRepository.save(agriculturalProperty);
+  }
+
+  isValidArea(
+    totalArea: number,
+    arableArea: number,
+    vegetationArea: number,
+  ): boolean {
+    return totalArea >= arableArea + vegetationArea;
+  }
+
+  async findOneById(
+    id: string,
+  ): Promise<FindOneByIdAgriculturalPropertyResponseDto> {
+    const agriculturalProperty =
+      await this.agriculturalPropertyRepository.findOne({
+        where: { id },
+      });
+
+    if (!agriculturalProperty) {
+      throw new NotFoundException(
+        `Agricultural Property with id ${id} not found`,
+      );
+    }
+
+    return agriculturalProperty;
+  }
+
+  async findAll(
+    input: FindAllAgriculturalPropertyQueryRequestDto,
+  ): Promise<FindAllAgriculturalPropertyResponseDto> {
+    const { take, skip, growerId } = input;
+    const [data, count] =
+      await this.agriculturalPropertyRepository.findAndCount({
+        where: {
+          grower: { id: growerId },
+        },
+        take,
+        skip,
+      });
+
+    return {
+      data,
+      count,
+    };
+  }
+}
