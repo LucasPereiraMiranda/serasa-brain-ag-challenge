70b661e79c9d0d25f562a4382b3c5de6b5fc8ffb
Lucas Pereira Miranda <lucas.pereira.miranda01@gmail.com>
2025-01-07 09:40:11 -0300

    feat: add crop module

---
+++ src/modules/harvest/crop.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Crop } from '../crop/crop.entity';
+import { CropController } from '../crop/crop.controller';
+import { CropService } from '../crop/crop.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Crop])],
+  controllers: [CropController],
+  providers: [CropService],
+})
+export class CropModule {}
