4352bd93761a9da204ebb16b02b57e0d5c4ca401
Lucas Pereira Miranda <lucas.pereira.miranda01@gmail.com>
2025-01-07 14:11:24 -0300

    feat: import entity and external module

--- src/modules/harvest/harvest.module.ts
+++ src/modules/harvest/harvest.module.ts
@@ -4,9 +4,15 @@ import { Harvest } from './harvest.entity';
 import { HarvestController } from './harvest.controller';
 import { HarvestService } from './harvest.service';
 import { AgriculturalPropertyModule } from '../agricutural-property/agricultural-property.module';
+import { CropModule } from '../crop/crop.module';
+import { HarvestToCrop } from '../harvest-to-crop/harvest-to-crop.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Harvest]), AgriculturalPropertyModule],
+  imports: [
+    TypeOrmModule.forFeature([Harvest, HarvestToCrop]),
+    AgriculturalPropertyModule,
+    CropModule,
+  ],
   controllers: [HarvestController],
   providers: [HarvestService],
 })
