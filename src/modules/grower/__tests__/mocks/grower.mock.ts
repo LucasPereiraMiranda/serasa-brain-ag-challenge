4928ccafe8c46aa1289f6346f71258c5354085be
Lucas Pereira Miranda <lucas.pereira.miranda01@gmail.com>
2025-01-08 09:24:45 -0300

    tests: coverage grower flow

---
+++ src/modules/grower/__tests__/mocks/grower.mock.ts
@@ -0,0 +1,11 @@
+export const growerCpfMock = {
+  id: 'id',
+  fullName: 'Lucas Pereira Miranda',
+  document: '73984910002',
+};
+
+export const growerCnpjMock = {
+  id: 'id',
+  fullName: 'Fazenda Lua',
+  document: '05278461000144',
+};
