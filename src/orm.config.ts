fd8037bb633c7431e5ea1b8063d872433d879a58
Lucas Pereira Miranda <lucas.pereira.miranda01@gmail.com>
2025-01-08 12:05:50 -0300

    chore: use database typeorm cache

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -1,8 +1,8 @@
 import * as path from 'path';
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { envConfig } from './env.load';
 
-export const ormConfig = {
+export const ormConfig: DataSourceOptions = {
   type: 'postgres' as const,
   synchronize: envConfig.db.synchronize,
   migrationsRun: envConfig.db.migrationsRun,
@@ -11,11 +11,15 @@ export const ormConfig = {
   port: envConfig.db.port,
   username: envConfig.db.username,
   password: envConfig.db.password,
-  autoLoadEntities: envConfig.db.autoLoadEntities,
   database: envConfig.db.database,
   migrations: [path.join(__dirname, '/migrations/*{.ts,.js}')],
   migrationsTableName: 'migrations',
   entities: [path.join(__dirname, '/**/*.entity{.ts,.js}')],
+  cache: {
+    type: 'database',
+    alwaysEnabled: false,
+    tableName: 'typeorm_cache',
+  },
 };
 
 const datasource = new DataSource(ormConfig);
